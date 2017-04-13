38a11ed23c926a3c99662d01e594587396d2e187
Anuj Gupta <junagupta19@gmail.com>
2017-04-13 21:32:41 +0530

    Added layout and function to make LoopJ Call

--- app/src/main/java/com/mate/android/loopj/MainActivity.java
+++ app/src/main/java/com/mate/android/loopj/MainActivity.java
@@ -2,12 +2,84 @@ package com.mate.android.loopj;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONObject;
+
+import cz.msebera.android.httpclient.Header;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    EditText etSearch;
+    Button btSearch;
+    TextView tvResponse;
+    AsyncHttpClient client = new AsyncHttpClient();
+    RequestParams requestParams = new RequestParams();
+    String BASE_URL = "http://www.omdbapi.com/?";
+    String jsonResponse;
+    String TAG = "MOVIE_TRIVIA";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etSearch = (EditText) findViewById(R.id.etSearchTerms);
+        btSearch = (Button) findViewById(R.id.btnSearch);
+        tvResponse = (TextView) findViewById(R.id.tvSearchResults) ;
+
+        btSearch.setOnClickListener(this);
+
     }
+
+    @Override
+    public void onClick(View v) {
+
+        String searchText = etSearch.getText().toString();
+        etSearch.setText("");
+        executeLoopJ(searchText);
+
+    }
+
+
+    public void executeLoopJ(String s){
+
+        requestParams.put("x",s);
+
+        client.get(BASE_URL,requestParams,new JsonHttpResponseHandler(){
+
+
+
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                super.onSuccess(statusCode, headers, response);
+                jsonResponse = response.toString();
+                Log.i(TAG, "onSuccess: " + jsonResponse);
+                tvResponse.setText(jsonResponse);
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                super.onFailure(statusCode, headers, throwable, errorResponse);
+                Log.e(TAG, "onFailure: " + errorResponse);
+                tvResponse.setText("Some error has occured");
+            }
+
+            public void onFinish(){
+                Log.i(TAG, "Finished: ");
+            }
+
+        });
+
+
+    }
+
 }
